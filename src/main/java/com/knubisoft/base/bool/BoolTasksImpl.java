df064863de0aeb91d2593e398024f5ba8ccf631c
sashababakop1489 <sashababakop1489@gmail.com>
2022-07-20 12:59:45 +0300

    BoolTask is done

--- src/main/java/com/knubisoft/base/bool/BoolTasksImpl.java
+++ src/main/java/com/knubisoft/base/bool/BoolTasksImpl.java
@@ -1,5 +1,7 @@
 package com.knubisoft.base.bool;
 
+import java.util.Arrays;
+
 public class BoolTasksImpl implements BoolTasks {
 
     @Override
@@ -24,40 +26,62 @@ public class BoolTasksImpl implements BoolTasks {
 
     @Override
     public boolean andFunction(int digit, String string) {
-//        int param = 0;
-//
-//        try {
-//            if(param = Integer.parseInt(string);
-//
-//                return true;
-//        }
-//        catch (Exception e){
+        if (string == null)
             return false;
-//        }
+        int number;
+        try {
+            number = Integer.parseInt(string);
+        } catch (Exception e){
+            return false;
+        }
+        return digit == number;
     }
 
     @Override
     public boolean orFunction(int digit, String string) {
-        return false;
+        if (string == null || string.equals(" "))
+            return false;
+        int number;
+        try {
+           number = Integer.parseInt(string);
+        } catch (Exception e) {
+            return false;
+        }
+        return digit == Integer.parseInt(string);
     }
 
     @Override
     public boolean andComplexFunction(int generatedFirstDigit, double generatedSecondDigit, int range) {
-        return false;
+        if(generatedFirstDigit == generatedSecondDigit && range == 3)
+            return true;
+        return generatedFirstDigit == generatedSecondDigit && range == 5;
     }
 
     @Override
     public boolean orComplexFunction(int generatedFirstDigit, double generatedSecondDigit, int generatedThirdDigit, int range) {
+        if (generatedFirstDigit == generatedSecondDigit || generatedFirstDigit == generatedThirdDigit)
+            if (range == 3)
+                return true;
+        if (generatedFirstDigit == generatedSecondDigit || generatedFirstDigit == generatedThirdDigit)
+            return range == 5;
         return false;
     }
 
     @Override
     public boolean isSameSizeArray(Object[] firstArray, Object... secondArray) {
-        return false;
+        if (firstArray == null || secondArray == null)
+            return false;
+        String first = Arrays.toString(firstArray);
+        String second = Arrays.toString(secondArray);
+        return first.length() == second.length();
     }
 
     @Override
     public boolean isSameCharactersCount(String username, String name, int maxLength) {
-        return false;
+        if (username == null || name == null)
+            return false;
+        if (username.equals("") || name.equals("") || maxLength == 0)
+            return false;
+        return username.length() == maxLength - 1 && name.length() == maxLength - 1;
     }
 }
