e1cc38b10ea72d8e18d3d90b5f1f36e60f3bcbbe
sashababakop1489 <sashababakop1489@gmail.com>
2022-08-08 19:55:48 +0300

    Chars in String is done

--- src/main/java/com/knubisoft/tasks/algorithm/charsinstring/CharsInStringImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/charsinstring/CharsInStringImpl.java
@@ -1,10 +1,25 @@
 package com.knubisoft.tasks.algorithm.charsinstring;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CharsInStringImpl implements CharsInString {
     @Override
     public Map<Character, Integer> charactersCount(String text) {
-        return null;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < text.length(); i++) {
+            Character value = text.charAt(i);
+            Integer count = map.get(value);
+            if (value == ' ')
+                continue;
+            if (count == null){
+                count = 1;
+                map.put(value, count);
+            } else {
+                count += 1;
+                map.put(value, count);
+            }
+        }
+        return map;
     }
 }
