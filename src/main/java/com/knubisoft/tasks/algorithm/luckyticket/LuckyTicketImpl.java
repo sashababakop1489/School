6633554f890fcc6a5acea84b968698553019c329
sashababakop1489 <sashababakop1489@gmail.com>
2022-08-08 23:49:29 +0300

    Lucky ticket is done(and some correct in test class)

--- src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
@@ -3,6 +3,17 @@ package com.knubisoft.tasks.algorithm.luckyticket;
 public class LuckyTicketImpl implements LuckyTicket {
     @Override
     public boolean checkIsLuckyTicket(String ticket) {
-        return false;
+        if (ticket == null || ticket.isBlank())
+            return false;
+        if (ticket.length() < 6 || ticket.length() > 8)
+            return false;
+        for (int i = 0; i < ticket.length(); i++) {
+            try {
+                int digit = Integer.parseInt(String.valueOf(ticket.charAt(i)));
+            }catch (Exception e) {
+                return false;
+            }
+        }
+        return true;
     }
 }
