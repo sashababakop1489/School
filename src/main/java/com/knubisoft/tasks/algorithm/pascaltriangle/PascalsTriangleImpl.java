c0758425947928c2047e7e3e4f257f2d3602031d
Aleksandr Babakov <sashababakop1489@gmail.com>
2022-09-12 18:30:24 +0300

    PascalsTriangle is done

--- src/main/java/com/knubisoft/tasks/algorithm/pascaltriangle/PascalsTriangleImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/pascaltriangle/PascalsTriangleImpl.java
@@ -1,11 +1,22 @@
 package com.knubisoft.tasks.algorithm.pascaltriangle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PascalsTriangleImpl implements PascalsTriangle {
 
     @Override
     public List<Integer> calculateLineOfPascalsTriangle(int lineNumber) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+
+        int prev = 1;
+        result.add(prev);
+
+        for (int i = 1; i < lineNumber; i++) {
+            int curr = (prev * (lineNumber - i)) / i;
+            result.add(curr);
+            prev = curr;
+        }
+        return result;
     }
 }
