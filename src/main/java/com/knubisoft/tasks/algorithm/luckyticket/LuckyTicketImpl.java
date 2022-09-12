5c40fbe2829709bf74b13266e8fc7843ed4c2ca6
Aleksandr Babakov <sashababakop1489@gmail.com>
2022-09-12 17:52:46 +0300

    ticket some correct and add sortKeysAndValues

--- src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
@@ -3,17 +3,18 @@ package com.knubisoft.tasks.algorithm.luckyticket;
 public class LuckyTicketImpl implements LuckyTicket {
     @Override
     public boolean checkIsLuckyTicket(String ticket) {
-        if (ticket == null || ticket.isBlank())
+        if (ticket == null || ticket.isBlank() || ticket.isEmpty())
             return false;
-        if (ticket.length() < 6 || ticket.length() > 8)
-            return false;
-        for (int i = 0; i < ticket.length(); i++) {
-            try {
-                int digit = Integer.parseInt(String.valueOf(ticket.charAt(i)));
-            }catch (Exception e) {
-                return false;
-            }
-        }
-        return true;
+        return ticket.matches("(\\d{6,8})");
+//        if (ticket.length() < 6 || ticket.length() > 8)
+//            return false;
+//        for (int i = 0; i < ticket.length(); i++) {
+//            try {
+//                int digit = Integer.parseInt(String.valueOf(ticket.charAt(i)));
+//            }catch (Exception e) {
+//                return false;
+//            }
+//        }
+//        return true;
     }
 }
--- src/main/java/com/knubisoft/tasks/algorithm/sorting/SortImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/sorting/SortImpl.java
@@ -2,12 +2,14 @@ package com.knubisoft.tasks.algorithm.sorting;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SortImpl implements Sort {
 
     @Override
     public Map<Integer, List<Integer>> sortKeysAndValues(Map<Integer, List<Integer>> input) {
-        return null;
+        input.entrySet().forEach(el ->el.getValue().sort(Integer::compareTo));
+        return input;
     }
 
 }
