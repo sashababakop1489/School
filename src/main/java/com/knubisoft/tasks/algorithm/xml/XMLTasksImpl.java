fc906f1b3225160cadf202d4125694c4c1fabb2c
Aleksandr Babakov <sashababakop1489@gmail.com>
2022-10-04 18:44:44 +0300

    XML

--- src/main/java/com/knubisoft/tasks/algorithm/xml/XMLTasksImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/xml/XMLTasksImpl.java
@@ -1,5 +1,6 @@
 package com.knubisoft.tasks.algorithm.xml;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.knubisoft.tasks.algorithm.ModelRoot;
 import lombok.SneakyThrows;
 import org.w3c.dom.Node;
@@ -7,6 +8,7 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.namespace.QName;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
@@ -15,17 +17,56 @@ public class XMLTasksImpl implements XMLTasks {
     @Override
     @SneakyThrows
     public ModelRoot parseXML(String xml) {
-        return null;
+        if (xml == null || xml.trim().isEmpty()) {
+            throw new NullPointerException();
+        }
+
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.findAndRegisterModules();
+
+        return xmlMapper.readValue(xml, ModelRoot.class);
     }
 
     @Override
+    @SneakyThrows
     public List<Integer> getAllIds(String xml) {
-        return null;
+        if (xml == null || xml.trim().isEmpty()) {
+            throw new NullPointerException();
+        }
+
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.findAndRegisterModules();
+
+        List<Integer> integerList = new ArrayList<>();
+        ModelRoot modelRoot = xmlMapper.readValue(xml, ModelRoot.class);
+
+        int counter = 0;
+        for (ModelRoot.Item integer : modelRoot.getItems()) {
+            integerList.add(counter, integer.id);
+            counter++;
+        }
+
+        return integerList;
     }
 
     @Override
+    @SneakyThrows
     public List<String> getNameWithIdMoreThan1(String xml) {
-        return null;
+        if (xml == null || xml.trim().isEmpty()) {
+            throw new NullPointerException();
+        }
+
+        List<String> stringList = new ArrayList<>();
+        XmlMapper xmlMapper = new XmlMapper();
+        ModelRoot modelRoot = xmlMapper.readValue(xml, ModelRoot.class);
+
+        for (ModelRoot.Item integer : modelRoot.getItems()) {
+            if (integer.id > 1) {
+                stringList.add(integer.name);
+            }
+        }
+
+        return stringList;
     }
 
     @SneakyThrows
