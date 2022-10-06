d4f4052051aaa9ef84e2b4de0ed27ac49ef9d6e9
Aleksandr Babakov <sashababakop1489@gmail.com>
2022-10-07 00:09:38 +0300

    Some correct

--- src/main/java/com/knubisoft/base/validation/ValidationTasksImpl.java
+++ src/main/java/com/knubisoft/base/validation/ValidationTasksImpl.java
@@ -1,138 +1,103 @@
 package com.knubisoft.base.validation;
 
 import com.knubisoft.base.validation.annotation.MaxLength;
-import com.knubisoft.base.validation.annotation.NotNull;
-import com.knubisoft.base.validation.annotation.PrimaryKey;
-import com.knubisoft.base.validation.annotation.ReferClass;
-import com.knubisoft.base.validation.dao.UserDao;
-import com.knubisoft.base.validation.dao.impl.UserDaoImpl;
-import org.reflections.Reflections;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.function.Function;
 
 public class ValidationTasksImpl implements ValidationTasks {
 
-    private final UserDao userDao = new UserDaoImpl();
-
     @Override
     public void validate(Object instance) {
-        Class<?> aClass = instance.getClass();
-        Field[] fields = aClass.getDeclaredFields();
-        for (Field field : fields) {
-            List<Annotation> annotationList = Arrays
-                    .stream(field.getDeclaredAnnotations())
-                    .peek(value -> {
-                        Class<?> annotationType = value.annotationType();
-                        if (annotationType == NotNull.class) {
-                            field.setAccessible(true);
-                            try {
-                                if (field.get(instance) == null) {
-                                    throw new NullPointerException("The field : "
-                                            + field.getName() + " is null!");
-                                }
-                            } catch (IllegalAccessException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                    })
-                    .collect(Collectors.toList());
-            for (Annotation annotation : annotationList) {
-                if (annotation.annotationType() == MaxLength.class) {
-                    MaxLength maxLengthAnnotation = (MaxLength) annotation;
-                    int maxLength = maxLengthAnnotation.maxLength();
-                    field.setAccessible(true);
-                    String fieldValue = null;
-                    try {
-                        fieldValue = (String) field.get(instance);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (fieldValue.length() > maxLength) {
-                        throw new IllegalArgumentException("The max length of " + field.getName()
-                                + " must be less or equal:" + maxLength + "!");
-                    }
-                }
-                if (annotation.annotationType() == PrimaryKey.class) {
-                    PrimaryKey primaryKeyAnnotation = (PrimaryKey) annotation;
-                    Long id;
-                    try {
-                        field.setAccessible(true);
-                        id = (Long) field.get(instance);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (userDao.getAllId().contains(id) && primaryKeyAnnotation.isPrimary()) {
-                        throw new IllegalArgumentException("This id: " + id
-                                + " is not uniq number!");
-                    }
-                }
-                if (annotation.annotationType() == ReferClass.class) {
-                    ReferClass referClassAnnotation = (ReferClass) annotation;
-                    Class<?> referClass = referClassAnnotation.referClass();
-                    Optional optional;
-                    Long id;
-                    try {
-                        Class<?> daoClass = Class.forName(UserDao.class.getPackageName() + "."
-                                + referClass.getSimpleName() + "Dao");
-                        field.setAccessible(true);
-                        id = (Long) field.get(instance);
-                        Set<Class<?>> subTypesOf = new Reflections(daoClass.getPackageName())
-                                .getSubTypesOf((Class<Object>) daoClass);
-                        Class<?> daoImpl = new ArrayList<>(subTypesOf).get(0);
-                        Method methodGetById = daoImpl.getMethod("getById", Long.class);
-                        optional = (Optional) methodGetById.invoke(daoImpl.getConstructor().newInstance(), id);
-                    } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
-                            InstantiationException | IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (optional.isEmpty()) {
-                        throw new IllegalArgumentException("The is absent by id:" + id + "!");
-                    }
-                }
+        Annotation[] annotations = instance.getClass().getDeclaredAnnotations();
+        Map<Field, String> resultMap = new LinkedHashMap<>();
+        for (Field f: instance.getClass().getDeclaredFields()) {
+            resultMap.put(f,checkForAnnotation(f, annotations, instance));
+        }
+    }
+
+    private String checkForAnnotation(Field f, Annotation[] annotations, Object instance) {
+        for (Annotation annotation: annotations) {
+            if(f.isAnnotationPresent(annotation.annotationType())) {
+                return validateField(f, annotation.annotationType(),instance) ? "valid" : "not valid";
             }
         }
+        return "No restrictions to field";
+    }
+
+    private boolean validateField(Field f, Class<? extends Annotation> annotationType, Object instance) {
+        Map<Class<? extends Annotation>, Function<Field, Boolean>> validateFunction = new LinkedHashMap<>();
+        validateFunction.put(MaxLength.class, field -> {
+            int fieldLength = 0;
+            try {
+                fieldLength = getGetterMethod(field.getName(), instance).invoke(instance).toString().length();
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+            int maxLength = f.getAnnotation(MaxLength.class).maxLength();
+            return fieldLength <= maxLength;
+        });
+        return validateFunction.get(annotationType).apply(f);
+    }
+
+    @SneakyThrows
+    private boolean checkMaxLength(Object instance) {
+        List<Field> fieldList = getFieldNames(instance, MaxLength.class);
+        for (Field f : fieldList) {
+            int fieldLength = getGetterMethod(f.getName(), instance).invoke(instance).toString().length();
+            int maxLength = f.getAnnotation(MaxLength.class).maxLength();
+            if (fieldLength > maxLength) {
+                System.out.println("Invalid length(" + fieldLength + ") for field " + f.getName() + ". Max length is " + maxLength);
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    @SneakyThrows
+    private Method getGetterMethod(String name, Object instance) {
+        return instance.getClass().getMethod("get" + StringUtils.capitalize(name));
+    }
+
+
+    private List<Field> getFieldNames(Object instance, Class<? extends Annotation> annotationClass) {
+        Field[] fields = instance.getClass().getDeclaredFields();
+        List<Field> fieldList = new ArrayList<>();
+        for (Field f : fields) {
+            if (f.isAnnotationPresent(annotationClass)) {
+                fieldList.add(f);
+            }
+        }
+        return fieldList;
     }
 
     @Override
     public User buildUser() {
-        return Stream.of(new User())
-                .peek(user -> user.setId(4L))
-                .peek(user -> user.setName("Bob"))
-                .peek(user -> user.setSurname("Babakov"))
-                .peek(user -> user.setMarried(true))
-                .peek(user -> user.setCountOfChildren(0))
-                .peek(user -> user.setCountOfPets(1))
-                .peek(user -> user.setFkUserGeneralDetails(4L))
-                .findFirst()
-                .orElseThrow();
+        User user = new User();
+        user.setId(12345L);
+        user.setName("Alex");
+        user.setSurname("Hnidash");
+        user.setMarried(false);
+        user.setCountOfChildren(0);
+        user.setCountOfPets(1);
+        user.setFkUserGeneralDetails(54321L);
+        return user;
     }
 
     @Override
     public UserGeneralDetails buildUserGeneralDetails() {
-        return Stream.of(new UserGeneralDetails())
-                .peek(ugd -> ugd.setId(4L))
-                .peek(ugd -> ugd.setCountry("Ukraine"))
-                .peek(ugd -> ugd.setOblast("Kharkiv"))
-                .peek(ugd -> ugd.setCity("Kharkiv"))
-                .peek(ugd -> ugd.setFkUserAddressDetails(4L))
-                .findFirst()
-                .orElseThrow();
+        return null;
     }
 
     @Override
     public UserAddressDetails buildUserAddressDetails() {
-        return Stream.of(new UserAddressDetails())
-                .peek(userAddressDetails -> userAddressDetails.setId(4L))
-                .peek(userAddressDetails -> userAddressDetails.setZipCode("61120"))
-                .peek(userAddressDetails -> userAddressDetails.setStreet("Vasil Stus"))
-                .peek(userAddressDetails -> userAddressDetails.setNumberOfHouse("5A"))
-                .findFirst()
-                .orElseThrow();
+        return null;
     }
 }
--- src/main/java/com/knubisoft/base/validation/dao/UserGeneralDetailsDao.java
+++
@@ -1,9 +0,0 @@
-package com.knubisoft.base.validation.dao;
-
-import com.knubisoft.base.validation.ValidationTasks;
-
-import java.util.Optional;
-
-public interface UserGeneralDetailsDao {
-    Optional<ValidationTasks.UserGeneralDetails> getById(Long id);
-}
